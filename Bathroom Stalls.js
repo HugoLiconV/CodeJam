d38636cea5072fb64588f6be539ed3752af6c0cc
Hugo <hugo_val28@hotmail.com>
2018-08-31 13:34:39 -0600

    add big int

--- Bathroom Stalls.js
+++ Bathroom Stalls.js
@@ -1,27 +1,28 @@
+const bigInt = require("big-integer");
 const fs = require("fs");
-const lines = fs.readFileSync("C-large-practice.in", "utf8").split("\n");
+const lines = fs.readFileSync("C-small-practice-1.in", "utf8").split("\n");
 
 const numOfCases = parseInt(lines.shift());
 let output = [];
 
 for (let i = 0; i < numOfCases; i++) {
   const input = lines[i].split(' ');
-  let numberOfStalls = parseInt(input[0]); // N stalls 
-  let numberOfPeople = parseInt(input[1]); // numberOfPeople people are about to enter the bathroom;
+  let numberOfStalls = bigInt(input[0]); // N stalls 
+  let numberOfPeople = bigInt(input[1]); // numberOfPeople people are about to enter the bathroom;
   const result = solveBathroomStalls(numberOfStalls, numberOfPeople);
   output.push(`Case #${(i + 1)}: ${result[0]} ${result[1]}`);
 }
 fs.writeFile('output.txt', output.join("\n"));
 
 function solveBathroomStalls(numberofStalls, numberOfPeople) {
-  if (numberOfPeople == 1) {
+  if (bigInt(numberOfPeople).equals(1)) {
     const arr = new Array();
-    arr[0] = Math.floor(numberofStalls / 2);
-    arr[1] = Math.floor((numberofStalls - 1) / 2);
+    arr[0] = bigInt(numberofStalls).divide(2);
+    arr[1] = bigInt(numberofStalls).minus(1).divide(2);
     return arr;
   }
-  if (numberOfPeople % 2 === 0) {
-    return solveBathroomStalls(Math.floor(numberofStalls / 2), Math.floor(numberOfPeople / 2));
+  if (bigInt(numberOfPeople).mod(2).equals(0)) {
+    return solveBathroomStalls(bigInt(numberofStalls).divide(2), bigInt(numberOfPeople).divide(2));
   }
-  return solveBathroomStalls(Math.floor((numberofStalls - 1) / 2), Math.floor(numberOfPeople / 2));
+  return solveBathroomStalls(bigInt(numberofStalls).minus(1).divide(2), bigInt(numberOfPeople).divide(2));
 }
